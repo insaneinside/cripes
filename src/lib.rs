9c902caa236d077c40295c661449c3ffe87a6ecb
Collin J. Sutton <collin.sutton@gmail.com>
2015-05-10 18:19:59 -0700

    fix for argument-order change to `std::slice::bytes::copy_memory`

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,14 @@
 #![feature(convert)]
 #![feature(unsafe_destructor)]
 
+use std::slice::bytes;
+
+/// Utility wrapper for `std::slice::bytes::copy_memory` that retains C's
+/// `memcpy` argument-order semantics.
+#[inline(always)]
+fn memcpy(dest: &mut [u8], src: &[u8]) {
+    bytes::copy_memory(src, dest)
+}
 
 #[macro_use]
 pub mod util;
