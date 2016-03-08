d705872f58b859f7a64e885a1b4ee7752dadcb57
Collin J. Sutton <collin.sutton@gmail.com>
2016-03-08 14:37:28 -0800

    util/mod.rs: make `memcpy` wrapper public
    Fixes issue introduced in commit 3bdaf7.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -27,7 +27,7 @@ macro_rules! panic_unless {
 /// Utility wrapper for `std::ptr::copy` that retains C's `memcpy`
 /// argument-order semantics.
 #[inline(always)]
-unsafe fn memcpy(dest: &mut [u8], src: &[u8]) {
+pub unsafe fn memcpy(dest: &mut [u8], src: &[u8]) {
     std::ptr::copy(src.as_ptr(), dest.as_mut_ptr(), std::cmp::min(src.len(), dest.len()))
 }
 
