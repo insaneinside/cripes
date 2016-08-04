9fac334f39a9122a78907b98502c7540639ab26b
Collin J. Sutton <collin.sutton@gmail.com>
2016-08-04 11:57:09 -0700

    src/lib.rs: explain the purpose of each feature attribute declaration

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,23 @@
 #![crate_name = "cripes"]
 #![warn(missing_docs)]
 
+// These features (`alloc`, `heap_api`, `unsize`, `coerce_unsized`, and
+// `core_intrinsics`) are used in the implementation of
+// `util::flex_box::FlexBox`, a reusable `Box`-like container.
 #![feature(alloc)]
 #![feature(heap_api)]
-
 #![feature(unsize)]
 #![feature(coerce_unsized)]
 #![feature(core_intrinsics)]
 
+// `range_contains` and `inclusive_range_syntax` are required in the `pattern`
+// module, where we use them for checking whether particular atoms are members
+// of a range in an atom class.
 #![feature(range_contains)]
 #![feature(inclusive_range_syntax)]
+
+// `stmt_expr_attributes` is used to enable debug output crate-wide using crate
+// feature "debug".
 #![feature(stmt_expr_attributes)]
 
 
