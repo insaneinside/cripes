1982ccadb2e707543112962956f626b219148ea4
Collin J. Sutton <collin.sutton@gmail.com>
2016-08-16 23:04:46 -0700

    pattern: defer to atom's `IsSubsetOf<Element<T>>` in `Element<T>`

--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -650,13 +650,7 @@ impl<T: Atom> set::IsSubsetOf<Element<T>> for Element<T> {
                 &Element::Sequence(_) |
                 &Element::Anchor(_)
                     => false, },
-            &Element::Atom(a) => match other {
-                &Element::Wildcard => true,
-                &Element::Atom(b) => a == b,
-                &Element::Class(ref c) => a.is_subset_of(c),
-                &Element::Repeat(ref r) => a.is_subset_of(r),
-                &Element::Union(ref u) => a.is_subset_of(u),
-                _ => false },
+            &Element::Atom(a) => a.is_subset_of(other),
             &Element::Class(ref c) => c.is_subset_of(other),
             &Element::Sequence(ref s) => s.is_subset_of(other),
             &Element::Union(ref u) => u.is_subset_of(other),
